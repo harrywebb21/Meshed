34fcf8957cfbab60a02ccb308d71c849af913c8c
harrywebb21 <harrywebb21@gmail.com>
2025-01-13 13:44:14 +0000

    changed landing page for main so people cant access yet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import GetStartedButton from "@/components/landing/buttons/getStarted/GetStartedButton";
+// import GetStartedButton from "@/components/landing/buttons/getStarted/GetStartedButton";
 import { GlassBall } from "@/components/Models";
 import Scene from "@/components/Scene";
 
@@ -7,9 +7,9 @@ export default function Home() {
     <div className=" w-screen h-dvh flex flex-col items-center justify-center">
       <div className="absolute z-10 gap-2 flex flex-col items-center justify-center">
         <h1 className="text-5xl font-black">MESHED.ART</h1>
-        <GetStartedButton />
-        {/* 
-          <p>COMING SOON</p> */}
+        {/* <GetStartedButton /> */}
+
+        <p>COMING SOON</p>
       </div>
       <Scene>
         <GlassBall position={[-2, 0, 3]} />
